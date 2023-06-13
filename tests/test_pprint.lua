e3ce4225b8a661d14a40844550539c686901be79
zachary <zlj19971222@outlook.com>
2023-06-13 22:34:53 +0800

    test(pprint): update test case

--- tests/test_pprint.lua
+++ tests/test_pprint.lua
@@ -1,15 +1,60 @@
-local pprint = loadfile('../pprint.lua')()
-
 local fmt = string.format
 
-local function test(name, func)
-    xpcall(function()
-        func()
-        print(fmt("[pass] %s", name))
-    end, function(err)
-        print(fmt("[fail] %s : %s", name, err))
-    end)
+local captured = {
+    o_print = print,
+    c_name = 'nil',
+    c_output = {},
+}
+
+function captured:pour()
+    for _, v in ipairs(self.c_output) do
+        self.o_print(string.rep('-', 50))
+        self.o_print(v.caller)
+        self.o_print(string.rep('-', 50))
+        self.o_print(v.content, '\n')
+    end
+
+    -- clear
+    self.c_output = {}
 end
+
+function captured:hijack_print(name)
+    if name ~= nil then
+        self.c_name = name
+    end
+
+    -- redefine global print
+    function print(...)
+        --local level = 2
+        --local info = debug.getinfo(level, "n")
+        --local caller = info.name or "unknown"
+
+        local output = {...}
+        table.insert(self.c_output, {
+            caller = self.c_name,
+            content = table.concat(output, "\t")
+        })
+    end
+end
+
+function captured:unhijack_print()
+    self.c_name = 'nil'
+    print = self.o_print
+end
+
+function captured:test(name, func)
+    self:hijack_print(name)
+    xpcall(
+        function()
+            func()
+            self.o_print(fmt("[pass] %s", name))
+        end,
+        function(err)
+            self.o_print(fmt("[fail] %s : %s", name, err))
+        end)
+    self:unhijack_print()
+end
+
 
 ------- ready -----------------------------------
 
@@ -29,7 +74,8 @@ local person = {
     },
     actions = {'sit', 'walk', 'run', say},
     2 ^ 26, -- a big number
-    list = a_list
+    list = a_list,
+    {}
 }
 
 function person.intro()
@@ -37,33 +83,20 @@ function person.intro()
 end
 
 ------- test case -------------------------------
+local pprint = loadfile('../pprint.lua')()
 
-test('PrettyPrinter.pprint', function()
-    pprint.PrettyPrinter():pprint(person)
+captured:test('PrettyPrinter:isrecursive', function()
+    assert(pprint.PrettyPrinter:isrecursive('string') == false)
+    assert(pprint.PrettyPrinter:isrecursive(123) == false)
+    assert(pprint.PrettyPrinter:isrecursive(false) == false)
+    assert(pprint.PrettyPrinter:isrecursive({'a', 'b'}) == true)
 end)
 
-test('PrettyPrinter.pprint:depth', function()
-    local printer = pprint.PrettyPrinter({
-        depth = 3,
-        scientific_notation = true
-    })
-
-    printer:pprint(person)
-end)
-
-test('PrettyPrinter.pprint:compact', function()
-    local printer = pprint.PrettyPrinter({
-        compact = true
-    })
-
-    printer:pprint(person)
-end)
-
-test('PrettyPrinter.pprint:isreadable', function()
+captured:test('PrettyPrinter:isreadable', function()
     assert(pprint.PrettyPrinter({
         depth = 3,
         scientific_notation = true
-    }):isreadable(test) == false)
+    }):isreadable(person) == false)
 
     assert(pprint.PrettyPrinter():isreadable(1) == true)
     assert(pprint.PrettyPrinter():isreadable('a') == true)
@@ -71,18 +104,45 @@ test('PrettyPrinter.pprint:isreadable', function()
 
     assert(pprint.PrettyPrinter():isreadable({1, 2, 3}) == true)
 
-    function abc(...)
-        -- no content
-    end
     assert(pprint.PrettyPrinter():isreadable({
-        abc = abc
+        abc = function (...)
+        -- no content
+        end}) == false)
+    assert(pprint.PrettyPrinter():isreadable({
+        [function () end] = 'abc'
     }) == false)
 
 end)
 
-test('pprint.isrecursive', function()
-    assert(pprint.isrecursive('string') == true)
-    assert(pprint.isrecursive(123) == true)
-    assert(pprint.isrecursive(false) == true)
-    assert(pprint.isrecursive({'a', 'b'}) == false)
+captured:test('PrettyPrinter.pprint:compact', function()
+    local printer = pprint.PrettyPrinter({
+        compact = true
+    })
+
+    printer:pprint(person)
 end)
+
+captured:test('PrettyPrinter.pprint:depth', function()
+    local printer = pprint.PrettyPrinter({
+        depth = 3,
+        scientific_notation = true
+    })
+
+    printer:pprint(person)
+end)
+
+captured:test('PrettyPrinter.pprint', function()
+    local tb = {}
+    local tmp = tb
+    for i = 1, 10 do
+        tmp['key']= {}
+        tmp = tmp.key
+    end
+
+    pprint.PrettyPrinter({depth=6}):pprint(tb)
+end)
+
+
+------- last ------------------------------------
+captured:pour()
+
